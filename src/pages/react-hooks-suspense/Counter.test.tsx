9949ddb7d01fee578c440f34f9a46e20b0983ab5
takasing <takaty2011@gmail.com>
2021-03-08 08:46:29 +0900

    localStorage, testing

--- src/pages/react-hooks-suspense/Counter.test.tsx
+++ src/pages/react-hooks-suspense/Counter.test.tsx
@@ -3,10 +3,27 @@ import React from 'react';
 import { Counter } from './Counter';
 import fireEvent from '@testing-library/user-event';
 
+afterEach(() => {
+  window.localStorage.removeItem('count');
+})
+
 test('counter increments the count', () => {
   const {container} = render(<Counter/>)
   const button = container.firstChild;
   expect(button.textContent).toBe('0')
   fireEvent.click(button)
   expect(button.textContent).toBe('1')
+})
+
+test('reads and updates localStorage', () => {
+  window.localStorage.setItem('count', '3');
+  const {container, rerender} = render(<Counter/>)
+  const button = container.firstChild;
+  expect(button.textContent).toBe('3')
+  fireEvent.click(button)
+  expect(button.textContent).toBe('4')
+  rerender(<Counter/>)
+  // 再描画しても4のままですよテスト
+  expect(button.textContent).toBe('4')
+  expect(window.localStorage.getItem('count')).toBe('4')
 })
--- src/pages/react-hooks-suspense/Counter.tsx
+++ src/pages/react-hooks-suspense/Counter.tsx
@@ -1,9 +1,24 @@
-import React, { useReducer, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+// const useCounter = ({initialState, step}: {initialState: number, step: number}) => {
+//   const [count, setCount] = useState(initialState)
+//   const increment = () => setCount(count => count + step)
+//   return {count, increment}
+// }
+const useCounter = (initialState: () => number) => {
+  const [count, setCount] = useState(initialState)
+  const increment = () => setCount(count => count + 1);
+  return {count, increment}
+}
 
 export const Counter: React.FC = () => {
-  const [count, setCount] = useState(0)
-  const incrementCount = () => setCount(currentCount => currentCount + 1)
+  // useStateの引数にすれば毎回呼び出さなくてよくなるため
+  const initialState = () => Number(window.localStorage.getItem('count') || 0);
+  const { count, increment } = useCounter(initialState);
+  useEffect(() => {
+    window.localStorage.setItem('count', String(count));
+  }, [count])
   return (
-    <button onClick={incrementCount}>{count}</button>
+    <button onClick={increment}>{count}</button>
   )
 }
